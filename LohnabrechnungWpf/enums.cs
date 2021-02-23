715a17e1eabb8051fb840e26713b496f02f88b85
TensorVince <77510703+TensorVince@users.noreply.github.com>
2021-02-23 23:19:12 +0100

    Enums eingecheckt

---
+++ LohnabrechnungWpf/enums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LohnabrechnungWpf
+{
+    public class Enums
+    {
+        public enum E_Bundesland
+        {
+            BadenWuerttemberg = 1,
+            Bayern = 2,
+            Berlin = 4,
+            Brandenburg = 8,
+            Bremen = 16,
+            Hamburg = 32,
+            Hessen = 64,
+            MecklenburgVorpommern = 128,
+            Niedersachsen = 256,
+            NordrheinWestfalen = 512,
+            RheinlandPfalz = 1024,
+            Saarland = 2048,
+            Sachsen = 4096,
+            SachsenAnhalt = 8192,
+            SchleswigHolstein = 16384,
+            Thueringen = 32768
+        }
+    }
+}
